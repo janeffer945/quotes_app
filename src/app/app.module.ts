036b7c1d05b27704d7948698ac25097ddab78cfd
janeffer945 <janeffernjeri945@gmail.com>
2022-04-22 00:38:54 +0300

    add quote-form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { AppVoteComponent } from './app-vote/app-vote.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
 
 @NgModule({
   declarations: [
     AppComponent, 
     QuoteComponent,
-    AppVoteComponent
+    AppVoteComponent,
+    QuoteFormComponent
      ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/quote-form/quote-form.component.ts
@@ -0,0 +1,24 @@
+import { Component,EventEmitter ,Output, OnInit } from '@angular/core';
+import { Quote } from '../quote';
+@Component({
+  selector: 'app-quote-form',
+  templateUrl: './quote-form.component.html',
+  styleUrls: ['./quote-form.component.css']
+})
+export class QuoteFormComponent implements OnInit {
+
+  quote = new Quote(0,"","","","",new Date(),0,0);
+  @Output() addQuote = new EventEmitter<Quote>();
+  // addQuote: any;
+  // newQuote: any;
+  
+
+  submitQuote (){
+    this.addQuote.emit(this.quote);
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
